94a7624e120cdd318dd8bea4ae4cdae8cb26ee70
Marc Richter <marc.richter@hyand.com>
2025-03-12 17:09:52 +0100

    Improved greeting output by adding the requested URL.

--- app.py
+++ app.py
@@ -2,13 +2,14 @@
 # See https://earthly.dev/blog/setup-reverse-proxy-kubernetes-nginx/
 # and #302354 for Details
 import os
-from flask import Flask
+from flask import Flask, request
+
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('*')
 def hello_world():
-    return f'Hello from {os.getenv("HELLO_NAME", "Flask")}!'
+    return f'Hello from {os.getenv("HELLO_NAME", "Flask")}, serving "{request.url}"!'
 
 
 if __name__ == '__main__':
